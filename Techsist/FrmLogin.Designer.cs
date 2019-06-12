1a4cbc03df80c41bb1002398a2ac4356f8397bac
Maria Thomas <malethr@gmail.com>
2019-06-12 14:24:45 -0600

    Create Login UI Form

---
+++ Techsist/FrmLogin.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Techsist
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblEmail = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.GrpSignIn = new System.Windows.Forms.GroupBox();
+            this.BtnSignIn = new System.Windows.Forms.Button();
+            this.PicLogo = new System.Windows.Forms.PictureBox();
+            this.GrpSignIn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblEmail
+            // 
+            this.LblEmail.AutoSize = true;
+            this.LblEmail.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEmail.ForeColor = System.Drawing.Color.White;
+            this.LblEmail.Location = new System.Drawing.Point(19, 24);
+            this.LblEmail.Name = "LblEmail";
+            this.LblEmail.Size = new System.Drawing.Size(46, 18);
+            this.LblEmail.TabIndex = 1;
+            this.LblEmail.Text = "Email";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPassword.ForeColor = System.Drawing.Color.White;
+            this.LblPassword.Location = new System.Drawing.Point(19, 73);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(76, 16);
+            this.LblPassword.TabIndex = 2;
+            this.LblPassword.Text = "Password";
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.Location = new System.Drawing.Point(22, 40);
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(252, 20);
+            this.TxtEmail.TabIndex = 3;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(22, 89);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(252, 20);
+            this.TxtPassword.TabIndex = 4;
+            // 
+            // GrpSignIn
+            // 
+            this.GrpSignIn.BackColor = System.Drawing.Color.Transparent;
+            this.GrpSignIn.Controls.Add(this.BtnSignIn);
+            this.GrpSignIn.Controls.Add(this.TxtPassword);
+            this.GrpSignIn.Controls.Add(this.TxtEmail);
+            this.GrpSignIn.Controls.Add(this.LblPassword);
+            this.GrpSignIn.Controls.Add(this.LblEmail);
+            this.GrpSignIn.Location = new System.Drawing.Point(250, 168);
+            this.GrpSignIn.Name = "GrpSignIn";
+            this.GrpSignIn.Size = new System.Drawing.Size(295, 172);
+            this.GrpSignIn.TabIndex = 5;
+            this.GrpSignIn.TabStop = false;
+            // 
+            // BtnSignIn
+            // 
+            this.BtnSignIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnSignIn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSignIn.ForeColor = System.Drawing.Color.White;
+            this.BtnSignIn.Location = new System.Drawing.Point(193, 133);
+            this.BtnSignIn.Name = "BtnSignIn";
+            this.BtnSignIn.Size = new System.Drawing.Size(80, 27);
+            this.BtnSignIn.TabIndex = 5;
+            this.BtnSignIn.Text = "SIGN IN";
+            this.BtnSignIn.UseVisualStyleBackColor = false;
+            // 
+            // PicLogo
+            // 
+            this.PicLogo.BackColor = System.Drawing.Color.Transparent;
+            this.PicLogo.Image = global::Techsist.Properties.Resources.Logo;
+            this.PicLogo.Location = new System.Drawing.Point(340, 35);
+            this.PicLogo.Name = "PicLogo";
+            this.PicLogo.Size = new System.Drawing.Size(120, 116);
+            this.PicLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicLogo.TabIndex = 0;
+            this.PicLogo.TabStop = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImage = global::Techsist.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(818, 520);
+            this.Controls.Add(this.GrpSignIn);
+            this.Controls.Add(this.PicLogo);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.GrpSignIn.ResumeLayout(false);
+            this.GrpSignIn.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicLogo;
+        private System.Windows.Forms.Label LblEmail;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox TxtEmail;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.GroupBox GrpSignIn;
+        private System.Windows.Forms.Button BtnSignIn;
+    }
+}
+
--- Techsist/FrmLogin.cs
+++ Techsist/FrmLogin.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace Techsist
 {
-    public partial class Form1 : Form
+    public partial class FrmLogin : Form
     {
-        public Form1()
+        public FrmLogin()
         {
             InitializeComponent();
         }
+
+      
     }
 }

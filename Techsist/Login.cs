83ece43447885e8cb1ff144a4cb3cb9de7967d27
Maria Thomas <malethr@gmail.com>
2019-06-12 15:15:58 -0600

    Add Login Class

---
+++ Techsist/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techsist
+{
+    // Start of Login Class
+    public class Login
+    {
+        // EmptyConstructor
+        public Login() { }
+
+        // Getter Setter for login variables
+        private string UserName { get; set; } = "";
+        private string Password { get; set; } = "";
+
+        // Assigned temporary username and password as administrator
+        private const string User = "admin";
+        private const string Pass = "password";
+
+        //Check if user input match to the login
+        public bool doesLoginMatch(string UserName, string Password)
+        {
+            if (UserName == User && Password == Pass)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }//End of Login Class
+}
